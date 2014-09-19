e020821dcd34c6be4074032ea2cd387bd51ffca7
hhaldimann <hunterhaldimann@sagetreellc.com>
2014-09-19 10:26:26 -0500

    target

---
+++ Target/Target.java
@@ -0,0 +1,26 @@
+import java.awt.Graphics2D;
+import java.awt.Rectangle;
+import java.awt.geom.Ellipse2D;
+import java.awt.geom.Line2D;
+import java.awt.geom.Point2D;
+
+public class Target
+{
+    private int xLeft;
+    private int yTop;
+    /**
+     * Default constructor for objects of class Target
+     */
+    public Target(int x, int y)
+    {
+        xLeft = x;
+        yTop = y;
+    }
+
+    public int sampleMethod(int y)
+    {
+        // put your code here
+        return x+y;
+    }
+
+}
