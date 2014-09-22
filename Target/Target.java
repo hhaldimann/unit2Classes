1a8418d44af35c95a83cf156d767b0e56ea342c5
hhaldimann <hunterhaldimann@sagetreellc.com>
2014-09-22 11:25:47 -0500

    almost working
    partially working..need to work on dimensions from xLeft and
    yTop...playing around with making targets bigger

--- Target/Target.java
+++ Target/Target.java
@@ -3,24 +3,45 @@ import java.awt.Rectangle;
 import java.awt.geom.Ellipse2D;
 import java.awt.geom.Line2D;
 import java.awt.geom.Point2D;
+import java.awt.Color;
 
 public class Target
 {
     private int xLeft;
     private int yTop;
-    /**
-     * Default constructor for objects of class Target
-     */
     public Target(int x, int y)
     {
         xLeft = x;
         yTop = y;
     }
-
-    public int sampleMethod(int y)
+    
+    public void draw(Graphics2D g2)
     {
-        // put your code here
-        return x+y;
+        Ellipse2D.Double bigBlack = new Ellipse2D.Double(xLeft + 110, yTop + 110, 150, 150);
+        Ellipse2D.Double mediumWhite = new Ellipse2D.Double(xLeft + 110, yTop + 110, 125, 125);
+        Ellipse2D.Double smallBlack = new Ellipse2D.Double(xLeft + 110, yTop + 110, 100, 100);
+        Ellipse2D.Double smallWhite = new Ellipse2D.Double(xLeft + 110, yTop + 110, 75, 75);
+        Ellipse2D.Double smallestBlack = new Ellipse2D.Double(xLeft + 110, yTop + 110, 50, 50);
+        
+        g2.draw(bigBlack);
+        g2.setColor(Color.RED);
+        g2.fill(bigBlack);
+       
+        g2.draw(mediumWhite);
+        g2.setColor(Color.WHITE);
+        g2.fill(mediumWhite);
+        
+        g2.draw(smallBlack);
+        g2.setColor(Color.BLUE);
+        g2.fill(smallBlack);
+        
+        g2.draw(smallWhite);
+        g2.setColor(Color.RED);
+        g2.fill(smallWhite);
+        
+        g2.draw(smallestBlack);
+        g2.setColor(Color.WHITE);
+        g2.fill(smallestBlack);
     }
 
 }
--- Target/TargetComponent.java
+++ Target/TargetComponent.java
@@ -2,32 +2,19 @@ import java.awt.Graphics;
 import java.awt.Graphics2D;
 import javax.swing.JComponent;
 
-/**
- * Write a description of class TargetComponent here.
- * 
- * @author hhaldimann 
- * @version 19 September 2014
- */
 public class TargetComponent extends JComponent
 {
-    private int x;
-
-    /**
-     * Default constructor for objects of class TargetComponent
-     */
-    public TargetComponent()
-    {
-        // initialise instance variables
-        x = 0;
-    }
-
     public void paintComponent(Graphics g)
     {
         Graphics2D g2 = (Graphics2D) g;
         Target target1 = new Target(0,0);
         
-        int x = getWidth() - 60;
-        int y = getHeight() - 30;
+        int x = getWidth() - 10;
+        int y = getHeight() - 10;
+        
+        Target target2 = new Target(x, y);
+        
+        target1.draw(g2);
+        target2.draw(g2);
     }
-
 }
